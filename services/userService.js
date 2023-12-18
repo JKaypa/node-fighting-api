10640d4f6902ef79edd3b53139bda63f8d7fa532
JKaypa <jose_kaypa@hotmail.com>
2023-12-17 23:30:36 -0500

    feat(user): Implement methods to work with user

--- services/userService.js
+++ services/userService.js
@@ -3,12 +3,41 @@ import { userRepository } from "../repositories/userRepository.js";
 class UserService {
   // TODO: Implement methods to work with user
 
+  getAll() {
+    const all = userRepository.getAll();
+    if (all) {
+      return all;
+    }
+    return null;
+  }
+
   search(search) {
     const item = userRepository.getOne(search);
     if (!item) {
       return null;
     }
     return item;
+  }
+
+  create(data) {
+    const { email, phoneNumber } = data;
+    const isUser = userRepository.getOne({ email, phoneNumber });
+
+    if (!isUser) {
+      const user = userRepository.create(data);
+      return user;
+    }
+    return null;
+  }
+
+  edit(id, data) {
+    const updatedData = userRepository.update(id, data);
+    return updatedData;
+  }
+
+  remove(id) {
+    const deleted = userRepository.delete(id);
+    return deleted;
   }
 }
 
