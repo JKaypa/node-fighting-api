e566c9be781c46b20ad50f3b0e615628a6c518ed
JKaypa <jose_kaypa@hotmail.com>
2023-12-17 23:34:54 -0500

    feat: Implement middleware that returns result of the query

--- middlewares/response.middleware.js
+++ middlewares/response.middleware.js
@@ -1,5 +1,10 @@
 const responseMiddleware = (req, res, next) => {
   // TODO: Implement middleware that returns result of the query
+  try {
+    res.json(res.data);
+  } catch (error) {
+    res.status(400).json(res.err);
+  }
   next();
 };
 
