b029408b86a96a44c165f1a0273086cfaebab52c
JKaypa <jose_kaypa@hotmail.com>
2023-12-17 23:33:26 -0500

    feat(user): Implement validatior for USER entity during creation and update

--- middlewares/user.validation.middleware.js
+++ middlewares/user.validation.middleware.js
@@ -2,12 +2,43 @@ import { USER } from "../models/user.js";
 
 const createUserValid = (req, res, next) => {
   // TODO: Implement validatior for USER entity during creation
+  const { firstName, lastName, email, phoneNumber, password } = req.body;
+  const regex = /[^a-zA-Z0-9]{3,}/;
+
+  let message = "Missing data: ";
+  let errorMessage = [];
+  if (!firstName || typeof firstName !== "string") {
+    errorMessage.push("first name");
+  }
+  if (!lastName || typeof lastName !== "string") {
+    errorMessage.push("last name");
+  }
+  if (!email || typeof email !== "string") {
+    errorMessage.push("email");
+  }
+  if (!phoneNumber || typeof phoneNumber !== "string") {
+    errorMessage.push("phone number");
+  }
+  if (!password || typeof password !== "string" || !regex.test(password)) {
+    errorMessage.push("password should have at least 3 symbols");
+  }
+
+  if (errorMessage.length > 0) {
+    message += errorMessage.join(", ");
+    return res.status(400).json({ error: true, message });
+  }
+
   next();
 };
 
 const updateUserValid = (req, res, next) => {
   // TODO: Implement validatior for user entity during update
-  next();
+  const { firstName, lastName, email, phoneNumber, password } = req.body;
+  if (firstName || lastName || email || phoneNumber || password) {
+    next();
+  }
+
+  return res.status(400).json({ error: true, message: "Missing data" });
 };
 
 export { createUserValid, updateUserValid };
