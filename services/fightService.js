26230d64b2082e9166fecd91dbf09e6b0fd34fe0
JKaypa <jose_kaypa@hotmail.com>
2023-12-18 19:08:16 -0500

    feat(fight): Implement methods to work with fights

--- services/fightService.js
+++ services/fightService.js
@@ -2,6 +2,45 @@ import { fightRepository } from "../repositories/fightRepository.js";
 
 class FightersService {
   // OPTIONAL TODO: Implement methods to work with fights
+
+  getAll() {
+    const all = fightRepository.getAll();
+    if (all) {
+      return all;
+    }
+    throw Error("Something failed!");
+  }
+
+  search(search) {
+    const item = fightRepository.getOne(search);
+    if (!item) {
+      throw Error("Fight not found!");
+    }
+    return item;
+  }
+
+  create(data) {
+    const fight = fightRepository.create(data);
+    return fight;
+  }
+
+  edit(id, data) {
+    const fight = fightRepository.getOne({ id });
+    if (!fight) {
+      throw Error("Fight not found!");
+    }
+    const updatedData = fightRepository.update(id, data);
+    return updatedData;
+  }
+
+  remove(id) {
+    const fight = fightRepository.getOne({ id });
+    if (!fight) {
+      throw new Error("Fight not found!");
+    }
+    const deleted = fightRepository.delete(id);
+    return deleted;
+  }
 }
 
 const fightersService = new FightersService();
