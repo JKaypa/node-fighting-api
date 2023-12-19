4a08aab83b7645a5f63291da3df6eae453812805
JKaypa <jose_kaypa@hotmail.com>
2023-12-18 19:09:42 -0500

    feat(fight): Implement route controller for fights

--- routes/fightRoutes.js
+++ routes/fightRoutes.js
@@ -1,13 +1,97 @@
 import { Router } from "express";
 import { fightersService } from "../services/fightService.js";
-import {
-  createUserValid,
-  updateUserValid,
-} from "../middlewares/user.validation.middleware.js";
+// import { createUserValid, updateUserValid } from "../middlewares/fight.validation.middleware.js";
 import { responseMiddleware } from "../middlewares/response.middleware.js";
 
 const router = Router();
 
 // OPTIONAL TODO: Implement route controller for fights
+
+router.get(
+  "/",
+  (_req, res, next) => {
+    try {
+      const fights = fightersService.getAll();
+      res.statusCode = 200;
+      res.data = fights;
+    } catch (error) {
+      res.statusCode = 500;
+      res.err = error.mesagge;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+
+router.get(
+  "/:id",
+  (req, res, next) => {
+    try {
+      const fight = fightersService.search(req.params);
+      res.statusCode = 200;
+      res.data = fight;
+    } catch (error) {
+      res.statusCode = 404;
+      res.err = error.message;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+
+router.post(
+  "/",
+  // createFighterValid,
+  (req, res, next) => {
+    try {
+      const fight = fightersService.create(req.body);
+      res.statusCode = 201;
+      res.data = fight;
+    } catch (error) {
+      res.statusCode = 400;
+      res.err = error.message;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+
+router.put(
+  "/:id",
+  // updateFighterValid,
+  (req, res, next) => {
+    try {
+      const updatedUser = fightersService.edit(req.params.id, req.body);
+      res.statusCode = 200;
+      res.data = updatedUser;
+    } catch (error) {
+      res.statusCode = 404;
+      res.err = error.message;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+
+router.delete(
+  "/:id",
+  (req, res, next) => {
+    try {
+      fightersService.remove(req.params.id);
+      res.statusCode = 200;
+      res.data = "fight deleted";
+    } catch (error) {
+      res.statusCode = 404;
+      res.err = error.message;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
 
 export { router };
